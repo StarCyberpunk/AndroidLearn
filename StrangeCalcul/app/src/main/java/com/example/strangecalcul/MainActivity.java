db0aa734c1bc76d5028e3028ba245654af8929a9
Emil <Starostin.emil@mail.ru>
2022-02-11 15:16:10 +0300

    StrangeCalc done

--- StrangeCalcul/app/src/main/java/com/example/strangecalcul/MainActivity.java
+++ StrangeCalcul/app/src/main/java/com/example/strangecalcul/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
 
@@ -16,16 +17,46 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private SeekBar seekbar;
     private ArrayList<Integer> numbers;
+    private int max=20;
+    private int min=1;
+    private int count =10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView=findViewById(R.id.ListViewNumbers);
         seekbar=findViewById(R.id.seekBar);
+        seekbar.setMax(max);
+
         numbers=new ArrayList<Integer>();
+        final ArrayAdapter<Integer> adapter=new ArrayAdapter<Integer>(getApplicationContext(),android.R.layout.simple_list_item_1,numbers);
+        listView.setAdapter(adapter);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                numbers.clear();
+                if(i<min){
+                    seekbar.setProgress(min);
 
+                }
+                else {
+                for (int j=min;j<=max;j++){
+                    numbers.add(seekbar.getProgress()*j);
+                }
+                }
+                adapter.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
+            }
 
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekbar.setProgress(10);
     }
 }
